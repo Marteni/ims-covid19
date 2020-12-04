def47574845274effba8f1b666ad481692035954
Marteni <36880529+Marteni@users.noreply.github.com>
2020-12-04 18:26:08 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/covid_19.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/covid_19.dir/main.cpp.obj"
+  "covid_19.exe"
+  "covid_19.exe.manifest"
+  "covid_19.pdb"
+  "libcovid_19.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/covid_19.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
